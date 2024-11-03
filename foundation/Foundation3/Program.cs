0efa6184d2fba216dfa7f9e3e044af16ebbfbbe4
arlettesteele <arlettesteele@gmail.com>
2024-11-02 18:25:21 -0600

    Final Project Completed

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 30, 20.0),
+            new Swimming(new DateTime(2022, 11, 3), 30, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
